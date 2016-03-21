324d26ae8edc3bb5273d244b235444e7914d11c3
Koan-Sin Tan <koansin.tan@gmail.com>
2016-03-21 17:15:06 +0800

    Patches to enable ATF on 8173 EVB (3/3)
    for ATF 0603-2016 (1.2+)
    
    SIP related modifications
    
    add support for BL33, kernel, and others

--- plat/mediatek/common/mtk_sip_svc.c
+++ plat/mediatek/common/mtk_sip_svc.c
@@ -27,121 +27,77 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-#include <assert.h>
-#include <console.h>
 #include <debug.h>
-#include <mmio.h>
-#include <mtk_plat_common.h>
 #include <mtk_sip_svc.h>
-#include <plat_sip_calls.h>
+#include <mtk_sip_private.h>
 #include <runtime_svc.h>
 #include <uuid.h>
 
 /* Mediatek SiP Service UUID */
 DEFINE_SVC_UUID(mtk_sip_svc_uid,
-		0xf7582ba4, 0x4262, 0x4d7d, 0x80, 0xe5,
-		0x8f, 0x95, 0x05, 0x00, 0x0f, 0x3d);
+		0x5aed33e0, 0xfd07, 0x11e3, 0xa3, 0xac,
+		0x08, 0x00, 0x20, 0x0c, 0x9a, 0x66);
 
-#pragma weak mediatek_plat_sip_handler
-uint64_t mediatek_plat_sip_handler(uint32_t smc_fid,
-				uint64_t x1,
-				uint64_t x2,
-				uint64_t x3,
-				uint64_t x4,
-				void *cookie,
-				void *handle,
-				uint64_t flags)
+
+/* Setup Standard Services */
+static int32_t sip_svc_setup(void)
 {
-	ERROR("%s: unhandled SMC (0x%x)\n", __func__, smc_fid);
-	SMC_RET1(handle, SMC_UNK);
+	/*
+	 * Invoke related module setup from here
+	 */
+
+	return 0;
 }
 
 /*
- * This function handles Mediatek defined SiP Calls */
-uint64_t mediatek_sip_handler(uint32_t smc_fid,
-			uint64_t x1,
-			uint64_t x2,
-			uint64_t x3,
-			uint64_t x4,
-			void *cookie,
-			void *handle,
-			uint64_t flags)
+ * Top-level Standard Service SMC handler. This handler will in turn dispatch
+ * calls to related SMC handler
+ */
+uint64_t sip_svc_smc_handler(uint32_t smc_fid,
+			     uint64_t x1,
+			     uint64_t x2,
+			     uint64_t x3,
+			     uint64_t x4,
+			     void *cookie,
+			     void *handle,
+			     uint64_t flags)
 {
-	uint32_t ns;
-
-	/* if parameter is sent from SMC32. Clean top 32 bits */
-	clean_top_32b_of_param(smc_fid, &x1, &x2, &x3, &x4);
-
-	/* Determine which security state this SMC originated from */
-	ns = is_caller_non_secure(flags);
-	if (!ns) {
-		/* SiP SMC service secure world's call */
-		;
-	} else {
-		/* SiP SMC service normal world's call */
-		switch (smc_fid) {
-#if MTK_SIP_SET_AUTHORIZED_SECURE_REG_ENABLE
-		case MTK_SIP_SET_AUTHORIZED_SECURE_REG: {
-			/* only use ret here */
-			uint64_t ret;
-
-			ret = mt_sip_set_authorized_sreg((uint32_t)x1,
-				(uint32_t)x2);
-			SMC_RET1(handle, ret);
-		}
-#endif
-#if MTK_SIP_KERNEL_BOOT_ENABLE
-		case MTK_SIP_KERNEL_BOOT_AARCH32:
-			boot_to_kernel(x1, x2, x3, x4);
-			SMC_RET0(handle);
-#endif
-		}
+	/*
+	 * Dispatch SIP calls to SIP Common handler and return its return value
+	 */
+	if (is_sip_fid(smc_fid)) {
+		return sip_smc_handler(smc_fid, x1, x2, x3, x4, cookie,
+				handle, flags);
 	}
 
-	return mediatek_plat_sip_handler(smc_fid, x1, x2, x3, x4,
-					cookie, handle, flags);
-
-}
-
-/*
- * This function is responsible for handling all SiP calls from the NS world
- */
-uint64_t sip_smc_handler(uint32_t smc_fid,
-			 uint64_t x1,
-			 uint64_t x2,
-			 uint64_t x3,
-			 uint64_t x4,
-			 void *cookie,
-			 void *handle,
-			 uint64_t flags)
-{
 	switch (smc_fid) {
-	case SIP_SVC_CALL_COUNT:
-		/* Return the number of Mediatek SiP Service Calls. */
-		SMC_RET1(handle,
-			 MTK_COMMON_SIP_NUM_CALLS + MTK_PLAT_SIP_NUM_CALLS);
+	case MTK_SIP_SVC_CALL_COUNT:
+		/*
+		 * Return the number of Standard Service Calls. PSCI is the only
+		 * standard service implemented; so return number of PSCI calls
+		 */
+		SMC_RET1(handle, MTK_SIP_SVC_NUM_CALLS);
 
-	case SIP_SVC_UID:
+	case MTK_SIP_SVC_UID:
 		/* Return UID to the caller */
 		SMC_UUID_RET(handle, mtk_sip_svc_uid);
 
-	case SIP_SVC_VERSION:
+	case MTK_SIP_SVC_VERSION:
 		/* Return the version of current implementation */
-		SMC_RET2(handle, MTK_SIP_SVC_VERSION_MAJOR,
-			MTK_SIP_SVC_VERSION_MINOR);
+		SMC_RET2(handle, MTK_SIP_VERSION_MAJOR, MTK_SIP_VERSION_MINOR);
 
 	default:
-		return mediatek_sip_handler(smc_fid, x1, x2, x3, x4,
-			cookie, handle, flags);
+		WARN("Unimplemented SIP Service Call: 0x%x \n", smc_fid);
+		SMC_RET1(handle, SMC_UNK);
 	}
 }
 
-/* Define a runtime service descriptor for fast SMC calls */
+/* Register Standard Service Calls as runtime service */
 DECLARE_RT_SVC(
-	mediatek_sip_svc,
-	OEN_SIP_START,
-	OEN_SIP_END,
-	SMC_TYPE_FAST,
-	NULL,
-	sip_smc_handler
+		sip_svc,
+		OEN_SIP_START,
+		OEN_SIP_END,
+		SMC_TYPE_FAST,
+		sip_svc_setup,
+		sip_svc_smc_handler
 );
