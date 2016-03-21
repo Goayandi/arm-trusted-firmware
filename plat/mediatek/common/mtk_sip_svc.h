3094a44168fc8bf9891906632f34377421b56469
Koan-Sin Tan <koansin.tan@gmail.com>
2016-03-21 17:15:06 +0800

    SIP related modifications
    add support for BL33, kernel, and others

--- plat/mediatek/common/mtk_sip_svc.h
+++ plat/mediatek/common/mtk_sip_svc.h
@@ -30,42 +30,70 @@
 #ifndef __PLAT_SIP_SVC_H__
 #define __PLAT_SIP_SVC_H__
 
-#include <stdint.h>
+/*******************************************************************************
+ * Defines for runtime services func ids
+ ******************************************************************************/
+/* SMC32 ID range from 0x82000000 to 0x82000FFF */
+/* SMC64 ID range from 0xC2000000 to 0xC2000FFF */
 
-/* SMC function IDs for SiP Service queries */
-#define SIP_SVC_CALL_COUNT		0x8200ff00
-#define SIP_SVC_UID			0x8200ff01
-/*					0x8200ff02 is reserved */
-#define SIP_SVC_VERSION			0x8200ff03
+/* For MTK SMC from Trustonic TEE */
+/* 0x82000000 - 0x820000FF & 0xC2000000 - 0xC20000FF */
+#define MTK_SIP_TBASE_HWUID_AARCH32       0x82000000
 
-/* Mediatek SiP Service Calls version numbers */
-#define MTK_SIP_SVC_VERSION_MAJOR	0x0
-#define MTK_SIP_SVC_VERSION_MINOR	0x1
+/* For MTK SMC from Boot-loader */
+/* 0x82000100 - 0x820001FF & 0xC2000100 - 0xC20001FF */
+#define MTK_SIP_BL_INIT_AARCH32           0x82000100
+#define MTK_SIP_BL_INIT_AARCH64           0xC2000100
 
-/* Number of Mediatek SiP Calls implemented */
-#define MTK_SIP_NUM_CALLS		4
+/* For MTK SMC from Kernel */
+/* 0x82000200 - 0x820002FF & 0xC2000200 - 0xC20002FF */
+#define MTK_SIP_KERNEL_TMP_AARCH32        0x82000200
+#define MTK_SIP_KERNEL_TMP_AARCH64        0xC2000200
+#define MTK_SIP_KERNEL_MCUSYS_WRITE_AARCH32 0x82000201
+#define MTK_SIP_KERNEL_MCUSYS_WRITE_AARCH64 0xC2000201
+#define MTK_SIP_KERNEL_MCUSYS_ACCESS_COUNT_AARCH32 0x82000202
+#define MTK_SIP_KERNEL_MCUSYS_ACCESS_COUNT_AARCH64 0xC2000202
+#define MTK_SIP_KERNEL_L2_SHARING_AARCH32  0x82000203
+#define MTK_SIP_KERNEL_L2_SHARING_AARCH64  0xC2000203
+#define MTK_SIP_KERNEL_WDT_AARCH32 0x82000204
+#define MTK_SIP_KERNEL_WDT_AARCH64 0xC2000204
+#define MTK_SIP_KERNEL_GIC_DUMP_AARCH32 0x82000205
+#define MTK_SIP_KERNEL_GIC_DUMP_AARCH64 0xC2000205
+#define MTK_SIP_KERNEL_DAPC_INIT_AARCH32 0x82000206
+#define MTK_SIP_KERNEL_DAPC_INIT_AARCH64 0xC2000206
 
-/* Mediatek SiP Service Calls function IDs */
 #define MTK_SIP_SET_AUTHORIZED_SECURE_REG	0x82000001
-#define MTK_SIP_PWR_ON_MTCMOS			0x82000402
-#define MTK_SIP_PWR_OFF_MTCMOS			0x82000403
-#define MTK_SIP_PWR_MTCMOS_SUPPORT		0x82000404
+#define MTK_SIP_PWR_ON_MTCMOS	0x82000402
+#define MTK_SIP_PWR_OFF_MTCMOS	0x82000403
+#define MTK_SIP_PWR_MTCMOS_SUPPORT             0x82000404
 
-/* Mediatek SiP Calls error code */
-enum {
-	MTK_SIP_E_SUCCESS = 0,
-	MTK_SIP_E_INVALID_PARAM = -1
-};
+/* For MTK SMC Reserved */
+/* 0x82000300 - 0x82000FFF & 0xC2000300 - 0xC2000FFF */
 
 /*
- * This function should be implemented in Mediatek SOC directory. It fullfills
- * MTK_SIP_SET_AUTHORIZED_SECURE_REG SiP call by checking the sreg with the
- * predefined secure register list, if a match was found, set val to sreg.
- *
- * Return MTK_SIP_E_SUCCESS on success, and MTK_SIP_E_INVALID_PARAM on failure.
+ * Number of SIP calls (above) implemented.
  */
-uint64_t mt_sip_set_authorized_sreg(uint32_t sreg, uint32_t val);
-uint64_t mt_sip_pwr_on_mtcmos(uint32_t val);
-uint64_t mt_sip_pwr_off_mtcmos(uint32_t val);
-uint64_t mt_sip_pwr_mtcmos_support(void);
+#define MTK_SIP_SVC_NUM_CALLS             2
+
+/*******************************************************************************
+ * Defines for SIP Service queries
+ ******************************************************************************/
+/* 0x82000000 - 0x8200FEFF is SIP service calls */
+#define MTK_SIP_SVC_CALL_COUNT      0x8200ff00
+#define MTK_SIP_SVC_UID             0x8200ff01
+/* 0x8200ff02 is reserved */
+#define MTK_SIP_SVC_VERSION         0x8200ff03
+/* 0x8200ff04 - 0x8200FFFF is reserved for future expansion */
+
+/* MTK SIP Service Calls version numbers */
+#define MTK_SIP_VERSION_MAJOR		0x0
+#define MTK_SIP_VERSION_MINOR		0x1
+
+/* The macros below are used to identify SIP calls from the SMC function ID */
+/* SMC32 ID range from 0x82000000 to 0x82000FFF */
+/* SMC64 ID range from 0xC2000000 to 0xC2000FFF */
+#define SIP_FID_MASK                0xf000u
+#define SIP_FID_VALUE               0u
+#define is_sip_fid(_fid) \
+	(((_fid) & SIP_FID_MASK) == SIP_FID_VALUE)
 #endif /* __PLAT_SIP_SVC_H__ */
--- plat/mediatek/mt8173/plat_sip_calls.c
+++ plat/mediatek/mt8173/plat_sip_calls.c
@@ -27,9 +27,81 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-#include <mmio.h>
+#include <arch.h>
+#include <arch_helpers.h>
+#include <assert.h>
+#include <bl_common.h>
+#include <context_mgmt.h>
+#include <runtime_svc.h>
+#include <debug.h>
 #include <mtk_sip_svc.h>
+#include <mtk_sip_error.h>
+#include <platform.h>
+#include <plat_private.h>
+#include <mmio.h>
+#include <mt8173_def.h>
 #include <mtcmos.h>
+#include <mtk_sip_private.h>
+
+extern entry_point_info_t *bl31_plat_get_next_kernel_ep_info(uint32_t type);
+
+#define MP0_MISC_CONFIG0	(MCUCFG_BASE + 0x0030)
+#define MP0_MISC_CONFIG9	(MCUCFG_BASE + 0x0054)
+
+/*******************************************************************************
+ * SMC Call for LK Jump
+ ******************************************************************************/
+struct kernel_info {
+	uint64_t pc;
+	uint64_t r0;
+	uint64_t r1;
+};
+
+static struct kernel_info k_info;
+
+static void save_kernel_info(uint64_t pc, uint64_t r0, uint64_t r1)
+{
+	k_info.pc=pc;
+	k_info.r0=r0;
+	k_info.r1=r1;
+}
+
+uint64_t get_kernel_info_pc(void)
+{
+	return k_info.pc;
+}
+
+uint64_t get_kernel_info_r0(void)
+{
+	return k_info.r0;
+}
+
+uint64_t get_kernel_info_r1(void)
+{
+	return k_info.r1;
+}
+
+void bl31_prepare_k64_entry(void)
+{
+	entry_point_info_t *next_image_info;
+	uint32_t image_type;
+
+	/* Determine which image to execute next */
+	image_type = NON_SECURE;
+
+	/* Program EL3 registers to enable entry into the next EL */
+	next_image_info = bl31_plat_get_next_kernel_ep_info(image_type);
+	assert(next_image_info);
+	assert(image_type == GET_SECURITY_STATE(next_image_info->h.attr));
+
+	INFO("BL3-1: Preparing for EL3 exit to %s world, Kernel\n",
+	(image_type == SECURE) ? "secure" : "normal");
+	INFO("BL3-1: Next image address = 0x%llx\n",
+		(unsigned long long)next_image_info->pc);
+	INFO("BL3-1: Next image spsr = 0x%x\n", next_image_info->spsr);
+	cm_init_context(read_mpidr_el1(), next_image_info);
+	cm_prepare_el3_exit(image_type);
+}
 
 /* Authorized secure register list */
 enum {
@@ -46,15 +118,14 @@ static const uint32_t authorized_sreg[] = {
 uint64_t mt_sip_set_authorized_sreg(uint32_t sreg, uint32_t val)
 {
 	uint64_t i;
-
 	for (i = 0; i < authorized_sreg_cnt; i++) {
 		if (authorized_sreg[i] == sreg) {
 			mmio_write_32(sreg, val);
-			return MTK_SIP_E_SUCCESS;
+			return SIP_SVC_E_SUCCESS;
 		}
 	}
 
-	return MTK_SIP_E_INVALID_PARAM;
+	return SIP_SVC_E_INVALID_PARAMS;
 }
 
 uint64_t mt_sip_pwr_on_mtcmos(uint32_t val)
@@ -63,9 +134,9 @@ uint64_t mt_sip_pwr_on_mtcmos(uint32_t val)
 
 	ret = mtcmos_non_cpu_ctrl(1, val);
 	if (ret)
-		return MTK_SIP_E_INVALID_PARAM;
+		return SIP_SVC_E_INVALID_PARAMS;
 	else
-		return MTK_SIP_E_SUCCESS;
+		return SIP_SVC_E_SUCCESS;
 }
 
 uint64_t mt_sip_pwr_off_mtcmos(uint32_t val)
@@ -74,12 +145,112 @@ uint64_t mt_sip_pwr_off_mtcmos(uint32_t val)
 
 	ret = mtcmos_non_cpu_ctrl(0, val);
 	if (ret)
-		return MTK_SIP_E_INVALID_PARAM;
+		return SIP_SVC_E_INVALID_PARAMS;
 	else
-		return MTK_SIP_E_SUCCESS;
+		return SIP_SVC_E_SUCCESS;
 }
 
-uint64_t mt_sip_pwr_mtcmos_support(void)
+uint64_t mt_sip_pwr_mtcmos_support()
 {
-	return MTK_SIP_E_SUCCESS;
+	return SIP_SVC_E_SUCCESS;
+}
+
+/*******************************************************************************
+ * SMC Call for Kernel MCUSYS register write
+ ******************************************************************************/
+
+static uint64_t mcusys_write_count = 0;
+static uint64_t sip_mcusys_write(unsigned int reg_addr, unsigned int reg_value)
+{
+	if((reg_addr & 0xFFFF0000) != (MCUCFG_BASE & 0xFFFF0000))
+		return SIP_SVC_E_INVALID_Range;
+
+    /* Perform range check */
+/*
+    if(( MP0_MISC_CONFIG0 <= reg_addr && reg_addr <= MP0_MISC_CONFIG9 ) ||
+       ( MP1_MISC_CONFIG0 <= reg_addr && reg_addr <= MP1_MISC_CONFIG9 )) {
+        return SIP_SVC_E_PERMISSION_DENY;
+    }
+*/
+	if(MP0_MISC_CONFIG0 <= reg_addr && reg_addr <= MP0_MISC_CONFIG9)
+		return SIP_SVC_E_PERMISSION_DENY;
+
+	mmio_write_32(reg_addr, reg_value);
+	dsb();
+
+	mcusys_write_count++;
+
+	return SIP_SVC_E_SUCCESS;
+}
+
+/*******************************************************************************
+ * SIP top level handler for servicing SMCs.
+ ******************************************************************************/
+uint64_t sip_smc_handler(uint32_t smc_fid,
+			 uint64_t x1,
+			 uint64_t x2,
+			 uint64_t x3,
+			 uint64_t x4,
+			 void *cookie,
+			 void *handle,
+			 uint64_t flags)
+{
+	uint64_t rc;
+	uint32_t ns;
+	atf_arg_t_ptr teearg;
+
+	/* Determine which security state this SMC originated from */
+	ns = is_caller_non_secure(flags);
+
+	//WARN("sip_smc_handler\n");
+	//WARN("id=0x%llx\n", smc_fid);
+	//WARN("x1=0x%llx, x2=0x%llx, x3=0x%llx, x4=0x%llx\n", x1, x2, x3, x4);
+
+	switch (smc_fid) {
+	case MTK_SIP_TBASE_HWUID_AARCH32:
+		teearg = (atf_arg_t_ptr)(uintptr_t)TEE_BOOT_INFO_ADDR;
+		if (ns)
+			SMC_RET1(handle, SMC_UNK);
+		SMC_RET4(handle, teearg->hwuid[0], teearg->hwuid[1],
+			 teearg->hwuid[2], teearg->hwuid[3]);
+		break;
+	case MTK_SIP_KERNEL_MCUSYS_WRITE_AARCH32:
+	case MTK_SIP_KERNEL_MCUSYS_WRITE_AARCH64:
+		rc = sip_mcusys_write(x1, x2);
+		break;
+	case MTK_SIP_KERNEL_MCUSYS_ACCESS_COUNT_AARCH32:
+	case MTK_SIP_KERNEL_MCUSYS_ACCESS_COUNT_AARCH64:
+		rc = mcusys_write_count;
+		break;
+	case MTK_SIP_KERNEL_TMP_AARCH32:
+		INFO("save kernel info\n");
+		save_kernel_info(x1, x2, x3);
+		INFO("end bl31_prepare_k64_entry...\n");
+		bl31_prepare_k64_entry();
+		INFO("el3_exit\n");
+		SMC_RET0(handle);
+		break;
+	case MTK_SIP_SET_AUTHORIZED_SECURE_REG:
+		rc = mt_sip_set_authorized_sreg((uint32_t)x1, (uint32_t)x2);
+		SMC_RET1(handle, rc);
+		break;
+	case MTK_SIP_PWR_ON_MTCMOS:
+		rc = mt_sip_pwr_on_mtcmos((uint32_t)x1);
+		SMC_RET1(handle, rc);
+		break;
+	case MTK_SIP_PWR_OFF_MTCMOS:
+		rc = mt_sip_pwr_off_mtcmos((uint32_t)x1);
+		SMC_RET1(handle, rc);
+		break;
+	case MTK_SIP_PWR_MTCMOS_SUPPORT:
+		rc = mt_sip_pwr_mtcmos_support();
+		SMC_RET1(handle, rc);
+		break;
+	default:
+		rc = SMC_UNK;
+		WARN("Unimplemented SIP Call: 0x%x \n", smc_fid);
+		break;
+	}
+
+    SMC_RET1(handle, rc);
 }
