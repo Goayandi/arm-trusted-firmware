a6faca433a5abdd2167d90674118de226594408e
Koan-Sin Tan <koansin.tan@gmail.com>
2016-04-18 17:20:05 +0800

    remove calling of all deprecated APIs
    now it's possbile to build PSCI for MT8173 with ERROR_DEPRECATED=1

--- plat/mediatek/mt8173/plat_sip_calls.c
+++ plat/mediatek/mt8173/plat_sip_calls.c
@@ -101,7 +101,7 @@ void bl31_prepare_k64_entry(void)
 	INFO("BL3-1: Next image address = 0x%llx\n",
 		(unsigned long long)next_image_info->pc);
 	INFO("BL3-1: Next image spsr = 0x%x\n", next_image_info->spsr);
-	cm_init_context(read_mpidr_el1(), next_image_info);
+	cm_init_my_context(next_image_info);
 	cm_prepare_el3_exit(image_type);
 }
 
