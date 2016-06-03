99ce58744a457350c3a3c2f65425bb57e1894657
Koan-Sin Tan <koansin.tan@gmail.com>
2016-06-03 15:02:55 +0800

    more checkings for OS Initiated mode

--- services/std_svc/psci/psci_main.c
+++ services/std_svc/psci/psci_main.c
@@ -293,7 +293,7 @@ int psci_features(unsigned int psci_fid)
 	if (psci_fid == PSCI_CPU_SUSPEND_AARCH32 ||
 			psci_fid == PSCI_CPU_SUSPEND_AARCH64) {
 		/*
-		 * The trusted firmware does not support OS Initiated Mode.
+		 * The trusted firmware supports OS Initiated Mode.
 		 */
 		return (FF_PSTATE << FF_PSTATE_SHIFT) |
 			((FF_SUPPORTS_OS_INIT_MODE) << FF_MODE_SUPPORT_SHIFT);
@@ -305,6 +305,35 @@ int psci_features(unsigned int psci_fid)
 
 int psci_set_suspend_mode(unsigned int mode)
 {
+	unsigned int idx;
+
+	if (mode > 1)
+		return PSCI_E_INVALID_PARAMS;
+
+	for (idx = 0; idx < PLATFORM_CORE_COUNT; idx++) {
+		INFO("  CPU Node : MPID 0x%lx, parent_node %d,"
+						" State 0x%x\n",
+					psci_cpu_pd_nodes[idx].mpidr,
+					psci_cpu_pd_nodes[idx].parent_node,
+					psci_get_cpu_local_state_by_idx(idx));
+
+		if (psci_get_cpu_local_state_by_idx(idx) >
+					(PLAT_MAX_PWR_LVL + 1))
+			return PSCI_E_DENIED;
+	}
+
+	INFO("mode = 0x%x\n", mode);
+	switch (mode) {
+	case 0:
+		if (psci_suspend_mode == 1)
+			psci_suspend_mode = 0;
+		break;
+	case 1:
+		if (psci_suspend_mode == 0)
+			psci_suspend_mode = 1;
+		break;
+	}
+
 	return PSCI_E_SUCCESS;
 }
 
@@ -371,11 +400,9 @@ uint64_t psci_smc_handler(uint32_t smc_fid,
 			/* We should never return from psci_system_reset() */
 
 		case PSCI_FEATURES:
-			WARN("PSCI FEATURE: 0x%lx,0x%x \n", x1, psci_features(x1));
 			SMC_RET1(handle, psci_features(x1));
 
 		case PSCI_SET_SUSPEND_MODE:
-			WARN("PSCI SUSPEND MODE: 0x%lx,0x%x \n", x1, psci_set_suspend_mode(x1));
 			SMC_RET1(handle, psci_set_suspend_mode(x1));
 
 		default:
