0ca5d0205578eb9c736eb247123325e0022c2c5b
Koan-Sin Tan <koansin.tan@gmail.com>
2016-11-22 18:32:15 +0800

    fix bakery lock problem

--- plat/mt6797/drivers/pwrc/plat_pwrc.c
+++ plat/mt6797/drivers/pwrc/plat_pwrc.c
@@ -37,7 +37,7 @@
  * TODO: Someday there will be a generic power controller api. At the moment
  * each platform has its own pwrc so just exporting functions is fine.
  */
-static bakery_lock_t pwrc_lock __attribute__ ((section("tzfw_coherent_mem")));
+DEFINE_BAKERY_LOCK(pwrc_lock);
 
 unsigned int plat_pwrc_get_cpu_wkr(unsigned long mpidr)
 {
